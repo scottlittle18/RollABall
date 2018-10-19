f73b7571a381ca6bfbc28913354dd84e59ae4c0e
Scott Little <scott.little@loop.colum.edu>
2018-10-19 12:03:17 -0500

    Add Canvas
    - Added text for score count and win count

--- Assets/Scripts/RollABallController.cs
+++ Assets/Scripts/RollABallController.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RollABallController : MonoBehaviour {
 
     [SerializeField]
     float moveForce;
     private float moveVertical, moveHorizontal;
+
+    public Text countText, winText;
+
+    int pickupCount;
     Rigidbody myRigidBody;
 
 	// Use this for initialization
 	void Start () {
         myRigidBody = GetComponent<Rigidbody>();
+        pickupCount = 0;
+        SetCountText();
+        winText.text = "";
 	}
 	
 	// Update is called once per frame
@@ -28,5 +36,26 @@ public class RollABallController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
 
         myRigidBody.AddForce(movement * moveForce);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            pickupCount++;
+            SetCountText();
+        }
+            
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + pickupCount.ToString();
+
+        if(pickupCount >= 13)
+        {
+            winText.text = "YOU WIN!!";
+        }
     }
 }
