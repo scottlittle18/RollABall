6a616b6fdcc15fffd78e520b6d3af8ab445f52d8
Scott Little <scott.little@loop.colum.edu>
2018-10-19 11:25:38 -0500

    Add Pickup Objects
    - Added Pickup Objects with the proper rotation and colour.
    - Create a prefab of the Pickup Objects

---
+++ Assets/Scripts/RollABallController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollABallController : MonoBehaviour {
+
+    [SerializeField]
+    float moveForce;
+    private float moveVertical, moveHorizontal;
+    Rigidbody myRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Move();
+	}
+
+    void Move()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        myRigidBody.AddForce(movement * moveForce);
+    }
+}
